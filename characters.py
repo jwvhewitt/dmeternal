44f6301bed397193c9225ad7829a69022fecb2bd
Joseph Hewitt <pyrrho12@yahoo.ca>
2013-10-11 10:41:31 +0900

    HP and MP working for all levels.

--- characters.py
+++ characters.py
@@ -1,17 +1,31 @@
 
 import stats
+import random
 
 class Level( object ):
     # Or, as we would say in a PnP RPG, a "class".
     # Keeping as much info as possible in the class attributes, so after pickling
     # a character the values can be changed here and the PC will automatically
     # use the new values.
-    def __init__( self, rank ):
-        self.rank = rank
+    def __init__( self, rank=1 ):
+        self.rank = 0
+        self.hp = 0
+        self.mp = 0
+        self.advance( rank )
         self.applied_rank = 0
     def get_stat_bonus( self, stat ):
         """Typical stat bonus is base bonus x rank"""
         return self.statline.get( stat , 0 ) * self.rank
+    def advance( self, ranks=1 ):
+        """Advance this level by the requested number of ranks."""
+        for r in range( ranks ):
+            self.rank += 1
+            if self.rank == 1:
+                self.hp = self.HP_DIE
+                self.mp = self.MP_DIE
+            else:
+                self.hp += random.randint( 1, self.HP_DIE )
+                self.mp += random.randint( 1, self.MP_DIE )
 
 class Warrior( Level ):
     name = 'Warrior'
@@ -19,13 +33,17 @@ class Warrior( Level ):
     requirements = { stats.STRENGTH: 11 }
     statline = { stats.PHYSICAL_ATTACK: 5, stats.MAGIC_ATTACK: 2, stats.MAGIC_DEFENSE: 3, \
         stats.AWARENESS: 3 }
+    HP_DIE = 12
+    MP_DIE = 4
 
 class Thief( Level ):
     name = 'Thief'
     desc = 'Highly skilled at stealth and disarming traps.'
     requirements = { stats.REFLEXES: 11 }
-    statline = { stats.PHYSICAL_ATTACK: 4, stats.MAGIC_ATTACK: 3, stats.MAGIC_DEFENSE: 4, \
-        stats.DISARM_TRAPS: 6, stats.STEALTH: 5, stats.AWARENESS: 4 }
+    statline = { stats.PHYSICAL_ATTACK: 4, stats.MAGIC_ATTACK: 4, stats.MAGIC_DEFENSE: 4, \
+        stats.DISARM_TRAPS: 6, stats.STEALTH: 5, stats.AWARENESS: 5 }
+    HP_DIE = 6
+    MP_DIE = 6
 
 class Bard( Level ):
     name = 'Bard'
@@ -33,6 +51,8 @@ class Bard( Level ):
     requirements = { stats.REFLEXES: 13, stats.INTELLIGENCE: 11, stats.CHARISMA: 13 }
     statline = { stats.PHYSICAL_ATTACK: 4, stats.MAGIC_ATTACK: 4, stats.MAGIC_DEFENSE: 3, \
         stats.DISARM_TRAPS: 4, stats.AWARENESS: 4 }
+    HP_DIE = 8
+    MP_DIE = 6
 
 class Priest( Level ):
     name = 'Priest'
@@ -40,6 +60,8 @@ class Priest( Level ):
     requirements = { stats.PIETY: 11 }
     statline = { stats.PHYSICAL_ATTACK: 4, stats.MAGIC_ATTACK: 4, stats.MAGIC_DEFENSE: 4, \
         stats.HOLY_SIGN: 5, stats.AWARENESS: 3 }
+    HP_DIE = 8
+    MP_DIE = 8
 
 class Mage( Level ):
     name = 'Mage'
@@ -47,6 +69,8 @@ class Mage( Level ):
     requirements = { stats.INTELLIGENCE: 11 }
     statline = { stats.PHYSICAL_ATTACK: 3, stats.MAGIC_ATTACK: 5, stats.MAGIC_DEFENSE: 4, \
         stats.AWARENESS: 3 }
+    HP_DIE = 4
+    MP_DIE = 10
 
 class Druid( Level ):
     name = 'Druid'
@@ -54,6 +78,8 @@ class Druid( Level ):
     requirements = { stats.TOUGHNESS: 9, stats.INTELLIGENCE: 11 }
     statline = { stats.PHYSICAL_ATTACK: 3, stats.MAGIC_ATTACK: 5, stats.MAGIC_DEFENSE: 3, \
         stats.AWARENESS: 4 }
+    HP_DIE = 6
+    MP_DIE = 10
 
 class Knight( Level ):
     name = 'Knight'
@@ -61,6 +87,8 @@ class Knight( Level ):
     requirements = { stats.STRENGTH: 11, stats.TOUGHNESS: 11, stats.PIETY: 17, stats.CHARISMA: 13 }
     statline = { stats.PHYSICAL_ATTACK: 5, stats.MAGIC_ATTACK: 2, stats.MAGIC_DEFENSE: 5, \
         stats.RESIST_LUNAR: 5, stats.AWARENESS: 2 }
+    HP_DIE = 10
+    MP_DIE = 4
 
 class Ranger( Level ):
     name = 'Ranger'
@@ -68,6 +96,8 @@ class Ranger( Level ):
     requirements = { stats.STRENGTH: 11, stats.REFLEXES: 13, stats.INTELLIGENCE: 11 }
     statline = { stats.PHYSICAL_ATTACK: 5, stats.MAGIC_ATTACK: 3, stats.MAGIC_DEFENSE: 3, \
         stats.DISARM_TRAPS: 3, stats.STEALTH: 5, stats.AWARENESS: 5 }
+    HP_DIE = 8
+    MP_DIE = 6
 
 class Necromancer( Level ):
     name = 'Necromancer'
@@ -75,6 +105,8 @@ class Necromancer( Level ):
     requirements = { stats.INTELLIGENCE: 13, stats.PIETY: 13 }
     statline = { stats.PHYSICAL_ATTACK: 3, stats.MAGIC_ATTACK: 5, stats.MAGIC_DEFENSE: 4, \
         stats.AWARENESS: 3 }
+    HP_DIE = 4
+    MP_DIE = 12
 
 class Samurai( Level ):
     name = 'Samurai'
@@ -82,6 +114,8 @@ class Samurai( Level ):
     requirements = { stats.STRENGTH: 15, stats.REFLEXES: 11, stats.INTELLIGENCE: 13, stats.PIETY: 11 }
     statline = { stats.PHYSICAL_ATTACK: 5, stats.MAGIC_ATTACK: 4, stats.MAGIC_DEFENSE: 3, \
         stats.KUNG_FU: 2, stats.AWARENESS: 3 }
+    HP_DIE = 10
+    MP_DIE = 6
 
 class Monk( Level ):
     name = 'Monk'
@@ -89,21 +123,25 @@ class Monk( Level ):
     requirements = { stats.TOUGHNESS: 15, stats.REFLEXES: 13, stats.PIETY: 13 }
     statline = { stats.PHYSICAL_ATTACK: 5, stats.MAGIC_ATTACK: 3, stats.MAGIC_DEFENSE: 4, \
         stats.KUNG_FU: 5, stats.NATURAL_DEFENSE: 4, stats.AWARENESS: 3 }
+    HP_DIE = 8
+    MP_DIE = 6
 
 class Ninja( Level ):
     name = 'Ninja'
     desc = 'They have a chance to slay living targets in a single hit.'
     requirements = { stats.STRENGTH: 13, stats.TOUGHNESS: 13, stats.REFLEXES: 13, \
-        stats.INTELLIGENCE: 13, stats.PIETY: 13, stats.CHARM: 13 }
-    statline = { stats.PHYSICAL_ATTACK: 4, stats.MAGIC_ATTACK: 3, stats.MAGIC_DEFENSE: 4, \
+        stats.INTELLIGENCE: 13, stats.PIETY: 13, stats.CHARISMA: 13 }
+    statline = { stats.PHYSICAL_ATTACK: 4, stats.MAGIC_ATTACK: 2, stats.MAGIC_DEFENSE: 3, \
         stats.DISARM_TRAPS: 4, stats.STEALTH: 5, \
         stats.NATURAL_DEFENSE: 4, stats.CRITICAL_HIT: 5, stats.AWARENESS: 4 }
+    HP_DIE = 8
+    MP_DIE = 4
 
 
 class Character(object):
     def __init__( self ):
-        statline = dict()
-        levels = []
+        self.statline = dict()
+        self.levels = []
 
     def get_stat( self , stat ):
         # Start with the basic stat value. This will probably be 0.
@@ -114,4 +152,35 @@ class Character(object):
         # Add bonuses from any equipment...
 
         return it
+
+    def rank( self ):
+        """Return the total ranks of this character's levels."""
+        return sum( l.rank for l in self.levels )
+
+    def max_hp( self ):
+        # Bonus is the number of extra points per two levels.
+        bonus = self.get_stat( stats.TOUGHNESS ) - 10
+        if bonus < 0:
+            bonus = 0
+        return sum( l.hp for l in self.levels ) + int( bonus * self.rank() / 2 )
+
+    def max_mp( self ):
+        # Bonus is the number of extra points per two levels.
+        bonus = self.get_stat( stats.PIETY ) - 10
+        if bonus < 0:
+            bonus = 0
+        return sum( l.mp for l in self.levels ) + int( bonus * self.rank() / 2 )
+
+if __name__ == '__main__':
+    pc = Character()
+    pc.levels.append( Ninja(3) )
+
+    print pc.rank()
+    print pc.get_stat( stats.PHYSICAL_ATTACK )
+
+    pc.statline[ stats.TOUGHNESS ] = 17
+    print "HP at 17:" , pc.max_hp()
+    pc.statline[ stats.TOUGHNESS ] = 10
+    print "HP at 10:" , pc.max_hp()
+
 
