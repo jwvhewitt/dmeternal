ebdfdf7a2fd05d5ebd83dc382510bd59e166001e
Joseph Hewitt <pyrrho12@yahoo.ca>
2013-11-20 16:43:03 +0900

    Changed terrain types to singletons.

--- maps.py
+++ maps.py
@@ -1,22 +1,79 @@
 import pygame
 import image
 
-GROUND1, GROUND2, WATER = range( 3 )
+# Enumerated constants for sprite sheets.
+SPRITE_GROUND, SPRITE_WALL, SPRITE_MISC, SPRITE_DECOR = range( 4 )
+
+class SingTerrain( object ):
+    # A singleton terrain class; use these objects as tokens for maps.
+    def __init__( self, ident, spritesheet = SPRITE_GROUND, frame = 0 ):
+        # ident should be the module-level name of this stat.
+        self.ident = ident
+        self.spritesheet = spritesheet
+        self.frame = frame
+    def render( self, screen, dest, view, data ):
+        view.sprites[ self.spritesheet ].render( screen, dest, self.frame )
+    def get_data( self, view, x, y ):
+        """Pre-generate display data for this tile."""
+        return None
+    def __str__( self ):
+        return self.ident
+    def __reduce__( self ):
+        return self.ident
+
+class GroundTerrain( SingTerrain ):
+    # A singleton terrain class; use these objects as tokens for maps.
+    def __init__( self, ident, spritesheet = SPRITE_GROUND, frame = 0, edge = None ):
+        # ident should be the module-level name of this stat.
+        self.ident = ident
+        self.spritesheet = spritesheet
+        self.frame = frame
+        self.edge = edge
+    def render( self, screen, dest, view, data ):
+        view.sprites[ self.spritesheet ].render( screen, dest, self.frame + data )
+    def get_data( self, view, x, y ):
+        """Pre-generate display data for this tile- frame offset."""
+        n = view.calc_floor_score( x , y , self.edge )
+        if n > 0:
+            n += 6
+        else:
+            n = view.get_pseudo_random() % 7
+        return n
+
+class WaterTerrain( SingTerrain ):
+    # A singleton terrain class; use these objects as tokens for maps.
+    def __init__( self, ident, spritesheet = SPRITE_GROUND, frame = 0 ):
+        # ident should be the module-level name of this stat.
+        self.ident = ident
+        self.spritesheet = spritesheet
+        self.frame = frame
+    def render( self, screen, dest, view, data ):
+        view.sprites[ self.spritesheet ].render( screen, dest, self.frame + ( view.phase // 10 ) % 2 )
+
+
+WATER = WaterTerrain( "WATER", frame = 56 )
+LOGROUND = GroundTerrain( "LOGROUND", frame = 28, edge = WATER )
+HIGROUND = GroundTerrain( "HIGROUND", edge = LOGROUND )
+
 
 class Tile( object ):
-    def __init__(self, floor=GROUND2, wall=None, decor=None, visible=False):
+    def __init__(self, floor=LOGROUND, wall=None, decor=None, visible=False):
         self.floor = floor
         self.wall = wall
         self.decor = decor
         self.visible = visible
 
+DEFAULT_SPRITES = { SPRITE_GROUND: "terrain_ground_forest.png", SPRITE_WALL: "", SPRITE_MISC: "", SPRITE_DECOR: "" }
 
 class Scene( object ):
     DELTA8 = ( (-1,-1), (0,-1), (1,-1), (-1,0), (1,0), (-1,1), (0,1), (1,1) )
-    def __init__(self,width=128,height=128):
+    def __init__(self,width=128,height=128,sprites=None):
         self.width = width
         self.height = height
         self.contents = []
+        self.sprites = DEFAULT_SPRITES.copy()
+        if sprites:
+            self.sprites.update( sprites )
         # Fill the map with empty tiles
         self.map = [[ Tile()
             for y in range(height) ]
@@ -33,43 +90,35 @@ class Scene( object ):
             return None
 
     def check_floors( self ):
-        """Make sure that GROUND1 never touches water."""
+        """Make sure that HIGROUND never touches water."""
         for x in range( self.width ):
             for y in range( self.height ):
-                if self.get_floor(x,y) == GROUND1:
+                if self.get_floor(x,y) == HIGROUND:
                     for dx,dy in self.DELTA8:
                         if self.get_floor(x+dx,y+dy) == WATER:
-                            self.map[x][y].floor = GROUND2
+                            self.map[x][y].floor = LOGROUND
                             break
 
 class SceneView( object ):
     def __init__( self, scene ):
-        self.ground_gfx = image.Image( "terrain_ground_forest.png", 54, 54 )
+        self.sprites = dict()
+        for k,v in scene.sprites.iteritems():
+            self.sprites[k] = image.Image( v, 54, 54 )
+
         self.scene = scene
         self.seed = 1
         self.x_off = 550
         self.y_off = 50
+        self.phase = 0
 
         self.map = []
         for x in range( scene.width ):
             self.map.append( [] )
             for y in range( scene.height ):
-                self.map[x].append( [] )
+                self.map[x].append( Tile() )
 
-                if scene.map[x][y].floor == GROUND1:
-                    n = self.calc_floor_score( x , y , GROUND2 )
-                    if n > 0:
-                        self.map[x][y].append( ( self.ground_gfx , 6 + n ) )
-                    else:
-                        self.map[x][y].append( ( self.ground_gfx , self.get_pseudo_random() % 7 ) )
-                elif scene.map[x][y].floor == GROUND2:
-                    n = self.calc_floor_score( x , y , WATER )
-                    if n > 0:
-                        self.map[x][y].append( ( self.ground_gfx , 34 + n ) )
-                    else:
-                        self.map[x][y].append( ( self.ground_gfx , 28 + self.get_pseudo_random() % 7 ) )
-                elif scene.map[x][y].floor == WATER:
-                    self.map[x][y].append( ( self.ground_gfx , 56 ) )
+                if scene.map[x][y].floor:
+                    self.map[x][y].floor = scene.map[x][y].floor.get_data( self, x, y )
 
 
     def calc_floor_score( self, x, y, terr ):
@@ -102,9 +151,11 @@ class SceneView( object ):
             for y in range( self.scene.height ):
                 sx = ( x * 27 ) - ( y * 27 ) + self.x_off
                 sy = ( y * 13 ) + ( x * 13 ) + self.y_off
-                for sprite,frame in self.map[x][y]:
-                    sprite.render( screen, (sx,sy), frame )
+                dest = pygame.Rect( sx, sy, 54, 54 )
 
+                if self.scene.map[x][y].floor:
+                    self.scene.map[x][y].floor.render( screen, dest, self, self.map[x][y].floor )
+        self.phase = ( self.phase + 1 ) % 600
 
 if __name__=='__main__':
     import random
@@ -153,14 +204,19 @@ if __name__=='__main__':
                 break
 
     myscene = Scene( 30 , 30 )
-    terrain_list = (GROUND1, GROUND1, GROUND1, GROUND1, GROUND1, GROUND1, GROUND1, GROUND1, GROUND1, GROUND1, GROUND1, GROUND1, GROUND2, GROUND2, WATER )
+    terrain_list = (HIGROUND, HIGROUND, HIGROUND, HIGROUND, HIGROUND, HIGROUND, HIGROUND, HIGROUND, HIGROUND, HIGROUND, HIGROUND, HIGROUND, LOGROUND, LOGROUND, WATER )
     for x in range( myscene.width ):
         for y in range( myscene.height ):
             myscene.map[x][y].floor = random.choice( terrain_list )
     myscene.check_floors()
     mysceneview = SceneView( myscene )
-    mysceneview( screen )
-    pygame.display.flip()
-    WaitAMinit()
+
+    while True:
+        ev = pygwrap.wait_event()
+        if ev.type == pygwrap.TIMEREVENT:
+            mysceneview( screen )
+            pygame.display.flip()
+        elif ( ev.type == pygame.MOUSEBUTTONDOWN ) or ( ev.type == pygame.QUIT ) or (ev.type == pygame.KEYDOWN):
+            break
 
 
