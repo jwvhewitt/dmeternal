a869fd852f4a255a853dc43f8742c9751a8c9888
Joseph Hewitt <pyrrho12@yahoo.ca>
2013-12-03 21:02:15 +0900

    Added new items.

--- items/lightarmor.py
+++ items/lightarmor.py
@@ -80,6 +80,68 @@ class HideArmor( Clothing ):
     avatar_frame = 0
     pants_frame = 5
     statline = stats.StatMod({ stats.PHYSICAL_DEFENSE: 15 })
-    mass = 95
+    mass = 105
+
+class StuddedLeather( Clothing ):
+    true_name = "Studded Leather Armor"
+    true_desc = ""
+    itemtype = LIGHT_ARMOR
+    avatar_image = "avatar_lightarmor.png"
+    avatar_frame = 11
+    pants_frame = 5
+    statline = stats.StatMod({ stats.PHYSICAL_DEFENSE: 15 })
+    mass = 115
+
+class LaminatedArmor( Clothing ):
+    true_name = "Laminated Armor"
+    true_desc = ""
+    itemtype = LIGHT_ARMOR
+    avatar_image = "avatar_lightarmor.png"
+    avatar_frame = 2
+    pants_frame = 3
+    statline = stats.StatMod({ stats.PHYSICAL_DEFENSE: 15 })
+    mass = 120
+
+class ChainShirt( Clothing ):
+    true_name = "Chain Shirt"
+    true_desc = ""
+    itemtype = LIGHT_ARMOR
+    avatar_image = "avatar_lightarmor.png"
+    avatar_frame = 13
+    pants_frame = 4
+    statline = stats.StatMod({ stats.PHYSICAL_DEFENSE: 15 })
+    mass = 75
+
+class ScaleMail( Clothing ):
+    true_name = "Scale Mail"
+    true_desc = ""
+    itemtype = LIGHT_ARMOR
+    avatar_image = "avatar_lightarmor.png"
+    avatar_frame = 12
+    pants_frame = 14
+    statline = stats.StatMod({ stats.PHYSICAL_DEFENSE: 20, stats.MAGIC_ATTACK:-10, stats.STEALTH:-5 })
+    mass = 135
+
+class Breastplate( Clothing ):
+    true_name = "Breastplate"
+    true_desc = ""
+    itemtype = LIGHT_ARMOR
+    avatar_image = "avatar_lightarmor.png"
+    avatar_frame = 15
+    pants_frame = 24
+    statline = stats.StatMod({ stats.PHYSICAL_DEFENSE: 20, stats.MAGIC_ATTACK:-5, stats.STEALTH:-10 })
+    mass = 140
+
+class ElvenChain( Clothing ):
+    true_name = "Elven Chainmail"
+    true_desc = ""
+    itemtype = LIGHT_ARMOR
+    avatar_image = "avatar_lightarmor.png"
+    avatar_frame = 10
+    pants_frame = 13
+    statline = stats.StatMod({ stats.PHYSICAL_DEFENSE: 20, stats.MAGIC_DEFENSE:10 })
+    mass = 125
+
+
 
 
--- items/swords.py
+++ items/swords.py
@@ -9,16 +9,16 @@ class Shortsword( Item ):
     avatar_image = "avatar_sword.png"
     avatar_frame = 0
     mass = 15
-    attackdata = Attack( (1,6,0) )
+    attackdata = Attack( (1,6,0), skill_mod=stats.REFLEXES )
 
 class Rapier( Item ):
     true_name = "Rapier"
-    true_desc = ""
+    true_desc = "This light, flexible sword relies on dexterity rather than brute strength."
     itemtype = SWORD
     avatar_image = "avatar_sword.png"
     avatar_frame = 28
     mass = 12
-    attackdata = Attack( (1,6,0), element = stats.RESIST_PIERCING )
+    attackdata = Attack( (1,8,0), element = stats.RESIST_PIERCING, skill_mod=stats.REFLEXES )
 
 class Longsword( Item ):
     true_name = "Longsword"
@@ -55,9 +55,28 @@ class Katana( Item ):
     true_desc = ""
     itemtype = SWORD
     avatar_image = "avatar_sword.png"
-    avatar_frame = 4
+    avatar_frame = 5
     mass = 45
     attackdata = Attack( (1,10,0) )
 
+class SilverShortsword( Item ):
+    true_name = "Silver Shortsword"
+    true_desc = "This blade was forged to slay unholy creatures. Its light weight favors an agile user."
+    itemtype = SWORD
+    avatar_image = "avatar_sword.png"
+    avatar_frame = 28
+    mass = 45
+    attackdata = Attack( (1,8,0), element=stats.RESIST_SOLAR, skill_mod=stats.REFLEXES )
+    statline = stats.StatMod({ stats.MAGIC_DEFENSE: 5 })
+
+class SilverLongsword( Item ):
+    true_name = "Silver Longsword"
+    true_desc = "This silver blade was forged with protective runes, to do battle against unholy creatures."
+    itemtype = SWORD
+    avatar_image = "avatar_sword.png"
+    avatar_frame = 6
+    mass = 45
+    attackdata = Attack( (1,10,0), element=stats.RESIST_SOLAR )
+    statline = stats.StatMod({ stats.MAGIC_DEFENSE: 5 })
 
 
