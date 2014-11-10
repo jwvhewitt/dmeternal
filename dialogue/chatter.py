e583aafa4209491815352990c2c94a79fb17ce75
Joseph Hewitt <pyrrho12@yahoo.ca>
2014-11-10 16:17:03 +0900

    Added object header for "Say"

--- dialogue/chatter.py
+++ dialogue/chatter.py
@@ -5,3 +5,18 @@
     this chatter stream has failed, which may or may not be a problem for
     the parent.
 """
+
+class Say( object ):
+    def __init__( self, msg, speaker, children=None, end_ok=True ):
+        self.msg = msg
+        self.speaker = speaker
+        self.children = children
+        self.end_ok = end_ok
+
+    def speak( self, actors ):
+        # We are going to Say.eet now.
+        # -Attempt to find a speaker
+        # -If not a good end, attempt to find a child.
+
+
+
