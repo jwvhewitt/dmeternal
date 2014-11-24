93229c94e062ea228a0d1e8aef2cf43d13ad7b66
Joseph Hewitt <pyrrho12@yahoo.ca>
2014-11-23 17:22:47 -0800

    Added spoony stub

--- narrator/advstub.py
+++ narrator/advstub.py
@@ -19,6 +19,37 @@ import worlds
 # adventure components. Yay ADVSTUB!
 #
 
+class SpoonyStub( Plot ):
+    LABEL = "STUB_SPOONY"
+    # Creates a supposedly plot-rich dungeon adventure.
+
+    def custom_init( self, nart ):
+        """Create the world + base camp."""
+        w = worlds.World()
+        nart.camp.contents.append( w )
+        self.register_element( "WORLD", w )
+        self.chapter = Chapter( end_rank=0, world=w )
+        self.add_first_locale_sub_plot( nart )
+
+        # Determine the chapter sizes.
+        if nart.end_rank < 7:
+            min_dungeon_size = 2
+        else:
+            min_dungeon_size = 3
+        num_dungeon,extra_points = divmod( nart.end_rank, min_dungeon_size )
+
+        # Step One- Generate a plot sequence, starting at the end and moving
+        #  backwards to the beginning.
+
+        # Step Two- Moving forward through the plot, connect the plot points.
+
+
+        for job in characters.PC_CLASSES:
+            self.add_sub_plot( nart, "RESOURCE_JOBTRAINER", PlotState( elements={"JOB":job} ) )
+
+        return True
+
+
 class BardicStub( Plot ):
     LABEL = "STUB_BARDIC"
     # Creates a simple, plot-free dungeon adventure.
